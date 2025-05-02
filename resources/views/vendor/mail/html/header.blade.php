f815750d6a36b929147eea2d629558ef103b759b
filip <fi.kwiatkowski@gmail.com>
2025-05-02 10:49:58 +0200

    zmiana url logo w header email

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
-            <img src="{{ asset('/storage/logo.png') }}" style="height: 75px;">
+            <img src="{{ asset('/images/logo.webp') }}" style="height: 75px;">
         </a>
     </td>
 </tr>
