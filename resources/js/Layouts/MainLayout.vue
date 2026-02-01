b74dcf28a4b9317b97a9b13806d40afab249640b
Filip Kwiatkowski <fi.kwiatkowski@gmail.com>
2026-02-01 11:18:14 +0100

    wylaczenia chatbota przy pomocy zmiennej

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -4,7 +4,7 @@
     <FlashMassage />
     <slot />
   </main>
-  <ChatWidget v-if="user" />
+  <ChatWidget v-if="user && CHATBOT_ACTIVE" />
 </template>
 
 <script setup>
@@ -14,4 +14,5 @@ import FlashMassage from '@/Components/UI/FlashMassage.vue'
 import { useAuthUser } from '@/Composables/useAuthUser'
 const user = useAuthUser()
 const ChatWidget = defineAsyncComponent(() => import('@/Chat/ChatWidget.vue'))
+const CHATBOT_ACTIVE = import.meta.env.VITE_CHATBOT_ACTIVE === 'true'
 </script>
